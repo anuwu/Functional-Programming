8f6126c277a0882b8da7aec21e7af680e4370e30
anwesh0304 <anwesh0304@gmail.com>
2020-04-05 12:34:26 +0530

    Master branch will now serve for functional programming.

---
+++ Scheme/Test/Append.rkt
@@ -0,0 +1,23 @@
+#lang racket
+(define (my-reverse lst)
+    (define (reverse-tail l acc)
+      (if (null? l) acc
+      (reverse-tail (cdr l) (cons (car l) acc))))
+    (reverse-tail lst '()))
+
+(define my-append1 (lambda (lst num)
+   (if (null? lst) (cons num '())
+        (cons (car lst) (my-append1 (cdr lst) num)))))
+
+(define (my-append2 lst num) (my-reverse (cons num (my-reverse lst))))
+
+(define (my-appendList1 lst1 lst2)
+    (if (null? lst1) lst2
+    (cons (car lst1) (my-appendList1 (cdr lst1) lst2))))
+
+(define (my-appendList2 lst1 lst2)
+	(define (app-list-help revlst1 acc)
+		(if (null? revlst1) acc
+		    (app-list-help (cdr revlst1) (cons (car revlst1) acc))
+		))
+	(app-list-help (my-reverse lst1) lst2))
---
+++ Scheme/Test/Factorial.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(define fact (lambda (n)
+     (if (= n 0) 1
+         (* n (fact (- n 1))))))
+
+(define fact-tail (lambda (n acc)
+    ( if (= n 0) acc
+       (fact-tail (- n 1) (* n acc)))))
+
+(define factWrap (lambda (num)
+    (define tail (lambda (acc n)
+      ( if (= n 0)
+           acc
+           (tail (* acc n) (- n 1)))))
+      (tail 1 num)))
+
+
