eed20e086403af4d5e852370ceea02f8c425d8df
anwesh0304 <anwesh0304@gmail.com>
2020-04-05 13:04:42 +0530

    Added my-reverse.

---
+++ Scheme/Test/My-reverse.rkt
@@ -0,0 +1,8 @@
+#lang racket
+; (my-reverse '(1 2 3 4 5))
+
+(define (my-reverse lst)
+    (define (reverse-tail l acc)
+      (if (null? l) acc
+      (reverse-tail (cdr l) (cons (car l) acc))))
+    (reverse-tail lst '()))
