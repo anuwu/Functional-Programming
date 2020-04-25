88b5d085ae7c68fdd25b1fc4bf87d0373dd50e11
anwesh0304 <anwesh0304@gmail.com>
2020-04-25 14:26:32 +0530

    Added complex number comparison.

--- Haskell/type.hs
+++ Haskell/type.hs
@@ -7,8 +7,22 @@ instance Show Complex where
 instance Eq Complex where
 	(==) (Complex x1 y1) (Complex x2 y2) = (x1 == x2) && (y1 == y2)
 
+instance Ord Complex where
+	(>) (Complex x1 y1) (Complex x2 y2) = absVal (Complex x1 y1) > absVal (Complex x2 y2)
+	(<=) (Complex x1 y1) (Complex x2 y2) = not ((Complex x1 y1) > (Complex x2 y2))
+	(<) (Complex x1 y1) (Complex x2 y2) = absVal (Complex x1 y1) < absVal (Complex x2 y2)
+	(>=) (Complex x1 y1) (Complex x2 y2) = not ((Complex x1 y1) < (Complex x2 y2))
+
 absVal :: Complex -> Double
 absVal (Complex a b) = sqrt (a^2 + b^2)
 
 (|==|) :: Complex -> Complex -> Bool
 (|==|) (Complex x1 y1) (Complex x2 y2) = absVal (Complex x1 y1) == absVal (Complex x2 y2)
+
+{-|
+solveQuad :: (Num a) => (a, a, a) -> Complex
+solveQuad (a, b, c)
+	| disc >= 0 = Complex ((-b + sqrt(disc))/(2*a) 0)
+	| otherwise = Complex ((-b/(2*a)) (sqrt(-disc))/(2*a))
+	where disc = b^2 - 4*a*c
+-}
