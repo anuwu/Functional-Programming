8532bc5db51e335d4f86e7909ec54992ee6893f8
Anwesh Bhattacharya <anwesh.bh@gmail.com>
2024-04-13 01:52:18 -0500

    Did upto all/any in chapter 7

--- Haskell/Learn You a Haskell/chap7.hs
+++ Haskell/Learn You a Haskell/chap7.hs
@@ -1,8 +1,26 @@
-mytranspose mat = transhelp (length mat) mat []
+mytranspose mat = transhelp (maximum $ map length mat) mat []
     where transhelp 0 mat lst = lst
-          transhelp n mat lst = transhelp (n-1) mat ([row !! (n-1) | row <- mat]:lst)
+          transhelp n mat lst = transhelp (n-1) mat ([row !! (n-1) | row <- mat, n <= length row]:lst)
 
 myintercalate l1 l2 = concat $ helper l1 l2                               
     where helper ls1 [] = [[]]                                              
           helper ls1 [x] = [x]                                              
           helper ls1 (el:ls2) = [el, ls1] ++ (helper ls1 ls2)
+
+myintersperse el [] = []
+myintersperse el [x] = [x]
+myintersperse el (x:xs) = let right = myintersperse el xs in (x:el:right)
+
+myconcat [] = []
+myconcat [x] = x
+myconcat (x:xs) = x ++ myconcat xs
+
+myconcatMap func lst = concat [map func ls | ls <- lst]
+
+myAnd :: [Bool] -> Bool
+myAnd = foldl (\x y -> x && y) True
+
+myOr :: [Bool] -> Bool
+myOr = foldl (\x y -> x || y) False
+myAll pred = and . map pred
+myAny pred = not . and . map (not . pred)
